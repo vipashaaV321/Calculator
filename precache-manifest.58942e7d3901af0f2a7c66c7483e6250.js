b5b31e519653cf3c0cda2cddf7f254dc8f04e215
vipasha <vips3201v@gmail.com>
2020-08-28 10:43:33 +0530

    Updates

---
+++ precache-manifest.58942e7d3901af0f2a7c66c7483e6250.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7320f0c68643e36fcc196c94a68d3c2",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "fc7b7a300afbbab02930",
+    "url": "/Calculator/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "044b63391f02fcc48082",
+    "url": "/Calculator/static/css/main.84ce9930.chunk.css"
+  },
+  {
+    "revision": "fc7b7a300afbbab02930",
+    "url": "/Calculator/static/js/2.07ca7f6c.chunk.js"
+  },
+  {
+    "revision": "cc19c1de1c8259cca6b9e836b96f27aa",
+    "url": "/Calculator/static/js/2.07ca7f6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "044b63391f02fcc48082",
+    "url": "/Calculator/static/js/main.8a11fb6a.chunk.js"
+  },
+  {
+    "revision": "0a79fa1d2af7875576c8",
+    "url": "/Calculator/static/js/runtime-main.8483c221.js"
+  },
+  {
+    "revision": "1164913b4932faae0fe871859b01684b",
+    "url": "/Calculator/static/media/calc.1164913b.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
